5f522051345a6478d9e46990985f42fe2a6c1336
Franco Jalil <jalilfranco9@gmail.com>
2023-08-12 14:50:58 -0300

    not pmreum

--- pricing/pricing.js
+++ pricing/pricing.js
@@ -1,4 +1,38 @@
+import { decodeJWTAndGetUsername } from '../profile.js'
 
+function ifNotPremium() {
+  let access_token = localStorage.getItem('access');
+
+  if (access_token) {
+    const url = 'http://localhost:8000/api/token/verify'
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        auth_token: access_token,
+      })
+    }).then(response => {
+      if (!response.ok) {
+        // Token verification failed, stay on the login page
+        console.log('Token verification failed');
+        return Promise.reject("Invalid token");
+      }
+      return response.json();
+    })
+      .then(data => {
+        let jwt_token = decodeJWTAndGetUsername(access_token)
+
+        if (jwt_token.status === 'member') {
+          window.location.href = "/home";
+        }
+      })
+      .catch(error => {
+        console.error('Token verification failed:', error);
+      });
+    }
+}
 
 function purchase() {
   
@@ -20,6 +54,9 @@ function purchase() {
 
 
 document.addEventListener('DOMContentLoaded', () => {
+
+  // No tiene que ser premium, si es, redirigirlo al home
+  ifNotPremium();
 
   // manejar el event listener directamente en el .html?
   let purchaseButton = document.getElementById('paypal-button-container-P-5MC57938GY016445NMTLGJ2Y');
--- profile.js
+++ profile.js
@@ -1,4 +1,4 @@
-function decodeJWTAndGetUsername(jwtToken) {
+export function decodeJWTAndGetUsername(jwtToken) {
   // In a real application, use the jsonwebtoken library to decode the JWT
   // Replace this with the actual JWT decoding logic for your application
   var decoded = jwtToken.split(".");
