ba02bb792d4fb4eadb41018fde354fec83269c61
Franco Jalil <jalilfranco9@gmail.com>
2023-08-15 19:49:40 -0300

    tokens menu

--- context.js
+++ context.js
@@ -1,12 +1,12 @@
 function decodeJWTAndGetUsername(jwtToken) {
-    // In a real application, use the jsonwebtoken library to decode the JWT
-    // Replace this with the actual JWT decoding logic for your application
-    var decoded = jwtToken.split(".");
+  // In a real application, use the jsonwebtoken library to decode the JWT
+  // Replace this with the actual JWT decoding logic for your application
+  var decoded = jwtToken.split(".");
 
-    var jwt_decoded = JSON.parse(atob(decoded[1]))
-    console.log(jwt_decoded)
-    return jwt_decoded;
-  }
+  var jwt_decoded = JSON.parse(atob(decoded[1]))
+  console.log(jwt_decoded)
+  return jwt_decoded;
+}
 
 
 
@@ -14,18 +14,18 @@ function decodeJWTAndGetUsername(jwtToken) {
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    // extraer data jwt aquí
-    const accessToken = localStorage.getItem('refresh');
-    console.log(accessToken)
-    // Decodificar el token utilizando jwt-decode
-    const decodedToken = decodeJWTAndGetUsername(accessToken);
+  // extraer data jwt aquí
+  const accessToken = localStorage.getItem('refresh');
+  console.log(accessToken)
+  // Decodificar el token utilizando jwt-decode
+  const decodedToken = decodeJWTAndGetUsername(accessToken);
+  const tokensElement = document.getElementById('tokens');
+  //document.getElementById('photo') decodedToken.picture
 
-    //document.getElementById('photo') decodedToken.picture
-
-    // Obtener el elemento de imagen por su id
-    const photoElement = document.getElementById('photo');
-
-    // Establecer el atributo "src" de la imagen con la URL del campo "picture"
-    photoElement.src = decodedToken.picture;
-
+  // Obtener el elemento de imagen por su id
+  const photoElement = document.getElementById('photo');
+  tokensElement.textContent = decodedToken.tokens;
+  // Establecer el atributo "src" de la imagen con la URL del campo "picture"
+  photoElement.src = decodedToken.picture;
+  tokensElement.textContent = decodedToken.tokens;
 });
